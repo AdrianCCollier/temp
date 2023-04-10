e16ff110747694f9563c8297d845f28feba63685
Adrian <AdrianCCollier@gmail.com>
2023-04-09 23:20:16 -0600

    update

--- calculator/app.js
+++ calculator/app.js
@@ -1,5 +1,37 @@
-const btnSeven = document.getElementById('btn-seven ');
+const display = document.getElementById('display');
+const AC = document.getElementById('AC');
+const plusButton = document.getElementById('plus');
+const twoButton = document.getElementById('2')
+const equalButton = document.getElementById('=');
+const minusButton = document.getElementById('-');
+const onButton = document.getElementById('Empty');
 
-btnSeven.addEventListener('click', () => {
-    console.log('yo');
-});
+let total = 0;
+let array = [];
+AC.onclick = function() {
+display.textContent = total;
+}
+
+plusButton.onclick = function() {
+    plus = +
+array.push(plus)
+}
+
+equalButton.onclick = function() {
+    display.textContent = total;
+}
+
+onButton.onclick = function() {
+    window.alert('you will die in 7 days');
+}
+
+twoButton.onclick = function() {
+    two = 2;
+array.push(two)
+}
+
+console.log(array);
+
+
+
+
--- wepaScript/wepaTracker.js
+++ wepaScript/wepaTracker.js
@@ -2,18 +2,16 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const audio = new Audio('wepaSound.mp3')
 
-async function getData() {
+async function getWepaStatus() {
   try {
-    const response = await axios.get(
-      'https://cs.wepanow.com/000nmsu146.html&filter='
-    )
-    const $ = cheerio.load(response.data)
-    const wepaAlert = $('.round-notif.red p, .round-notif.yellow p')
+    const response = await axios.get('https://cs.wepanow.com/000nmsu146.html&filter=');
+    const $ = cheerio.load(response.data);
+    const wepaAlert = $('.round-notif.red p, .round-notif.yellow p');
 
     if (wepaAlert.length) {
-      const printerCount = wepaAlert.text().trim()
+      const printerCount = wepaAlert.text().trim();
       console.log('Printer count:', printerCount)
-      audio.play()
+      audio.play();
       document.querySelector('#status-button').classList.remove('green')
       document.querySelector('#status-button').classList.add('red')
     } else {
@@ -24,8 +22,8 @@ async function getData() {
       document.querySelector('#status-button').classList.add('green')
     }
   } catch (error) {
-    console.error('Error while scraping data:', error)
+    console.error('Error while retrieving data:', error)
   }
 }
 
-getData()
+getWepaStatus();
